25d53bd60803444282d61244732a1b734d6a36a6
miguel5219 <5219@holbertonstudents.com>
2022-10-25 15:59:22 -0500

    add created public instances

--- models/base_model.py
+++ models/base_model.py
@@ -2,5 +2,16 @@
 """ class base """
 
 
+from datetime import datetime
+import uuid
+
+
 class BaseModel():
-    pass
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
