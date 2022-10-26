5ca3b3ffcb4ad76b6c4e6c9d72e6297626ff7fa1
miguel5219 <5219@holbertonstudents.com>
2022-10-26 14:30:43 -0500

    fixed ubication of file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" test for task 3"""
+
+
+from models.base_model import BaseModel
+from datetime import datetime
+import unittest
+
+
+class TestsBaseModel(unittest.TestCase):
+    def testBm(self):
+        myModel = BaseModel()
+        bm = myModel.update_at
+        bm.save()
+        myModel1 = bm.update_at
+        self.assertNotEqual(myModel, myModel)
