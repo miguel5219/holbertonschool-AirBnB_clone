a6113e46b2f7471e61692e0479b7e97a8987bc6f
miguel5219 <5219@holbertonstudents.com>
2022-10-26 11:15:56 -0500

    .

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,9 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
+                if key == "created_at" or  key == "update_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__"
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
