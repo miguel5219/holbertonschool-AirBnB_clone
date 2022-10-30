7a1e8299ae9a7953d2c0b6c7b6969cd36bbf4072
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-29 19:17:14 -0500

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,13 @@
 
 from datetime import datetime
 import uuid
-import models
+import models import storage
 
 
 class BaseModel():
-
+''' Class BaseModel that defines all common attributes/methods for other classes '''
     def __init__(self, *args, **kwargs):
-        if not kwargs == {}:
+        if kwargs is None and len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
@@ -23,10 +23,11 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-        models.storage.new(self)
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
+        ''' __str__ method that returns a class description '''
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
@@ -35,7 +36,7 @@ class BaseModel():
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
@@ -43,7 +44,7 @@ class BaseModel():
         returns a dictionary containing all keys/values.
         """
         dictionary = self.__dict__.copy()
-        dictionary["__class__"] = self.__class__.__name__
         dictionary["created_at"] = dictionary["created_at"].isoformat()
         dictionary["updated_at"] = dictionary["updated_at"].isoformat()
+        dictionary["__class__"] = self.__class__.__name__
         return dictionary
