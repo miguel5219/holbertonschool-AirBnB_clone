fe206a376336ff6a3ef09db959fcdda6dc1edc67
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-29 20:45:39 -0500

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,24 @@ from models import storage
 
 
 class BaseModel():
-    ''' Class BaseModel that defines all common attributes/methods for other classes '''
+    '''
+    Class BaseModel that defines all common attributes/methods for other classes
+    '''
+
     def __init__(self, *args, **kwargs):
+        '''
+        Class constructor
+        '''
+
         if kwargs is None and len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
 
-            self.created_at = datetime.strptime(
-                self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(
-                self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.strptime(self.created_at,
+                                                '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(self.updated_at,
+                                                '%Y-%m-%dT%H:%M:%S.%f')
 
         else:
             self.id = str(uuid.uuid4())
@@ -28,7 +35,7 @@ class BaseModel():
 
     def __str__(self):
         ''' __str__ method that returns a class description '''
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
@@ -44,7 +51,7 @@ class BaseModel():
         returns a dictionary containing all keys/values.
         """
         dictionary = self.__dict__.copy()
-        dictionary["created_at"] = dictionary["created_at"].isoformat()
-        dictionary["updated_at"] = dictionary["updated_at"].isoformat()
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
         dictionary["__class__"] = self.__class__.__name__
         return dictionary
