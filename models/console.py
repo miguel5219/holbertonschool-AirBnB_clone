0274b5b6b4692bb7059086c3e774d1039e2eacf7
miguel5219 <5219@holbertonstudents.com>
2022-10-31 17:58:02 -0500

    created console first step

---
+++ models/console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""modulecontains the class HBNBCommand"""
+
+import cmd
+import models.base_model
+import models
+
+
+class HBNBCommand(cmd.Cmd):
+    """ class to implements a basic prompt to handle objects"""
+
+    def do_EOF(self, line):
+        return True
+
+    def do_quit(self, arg):
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
