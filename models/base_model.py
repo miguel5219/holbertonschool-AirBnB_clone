8e3247d17c49dfd4bad30233ac646721f576244f
miguel5219 <5219@holbertonstudents.com>
2022-10-31 11:59:47 -0500

    fixed code

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 from datetime import datetime
 import uuid
-from models import storage
+import models
 
 
 class BaseModel():
@@ -17,7 +17,7 @@ class BaseModel():
         Class constructor
         '''
 
-        if kwargs is None and len(kwargs) > 0:
+        if not kwargs == {}:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
@@ -30,12 +30,12 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.updated_at = self.created_at
+        models.storage.new(self)
 
     def __str__(self):
         ''' __str__ method that returns a class description '''
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
@@ -43,15 +43,18 @@ class BaseModel():
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
         Public instance methods that
         returns a dictionary containing all keys/values.
         """
-        dictionary = self.__dict__.copy()
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
-        dictionary["__class__"] = self.__class__.__name__
+        dictionary = {}
+        dictionary['__class__'] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime):
+                dictionary[key] = value.isoformat()
+            else:
+                dictionary[key] = value
         return dictionary
