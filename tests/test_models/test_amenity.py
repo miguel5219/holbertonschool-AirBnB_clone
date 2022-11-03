1294b15f7506093ef3922bd4ff11936f7465b04d
miguel5219 <5219@holbertonstudents.com>
2022-11-03 18:01:46 -0500

    add test class Amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Unittest for class Amenity """
+
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+
+    def test_amenity_name(self):
+        review = Amenity()
+        self.assertTrue(hasattr(review, 'name'))
+        self.assertEqual(review.name, "")
+        review.name = "mirador de san felix"
+        self.assertDictEqual(review.name, "mirador de san felix")
